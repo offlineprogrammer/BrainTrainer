e0e7179574f0bdbdadd48dd1c1687ed98ab8afe0
offlineProgrammer <mmalaka@gmail.com>
2019-12-26 17:49:13 -0800

    Hide Start Button

--- app/src/main/java/com/offlineprogrammer/braintrainer/MainActivity.java
+++ app/src/main/java/com/offlineprogrammer/braintrainer/MainActivity.java
@@ -3,6 +3,9 @@ package com.offlineprogrammer.braintrainer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void start(View view){
+        Log.i("goButton Clicked","Hide");
+        Button goButton = findViewById(R.id.goButton);
+        goButton.setVisibility(View.VISIBLE);
     }
 }
