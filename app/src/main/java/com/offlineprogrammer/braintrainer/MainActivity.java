5f4cd6a7b4ee35752a402be6140abd92922eae72
offlineProgrammer <mmalaka@gmail.com>
2019-12-26 19:59:57 -0800

    Random + Answers

--- app/src/main/java/com/offlineprogrammer/braintrainer/MainActivity.java
+++ app/src/main/java/com/offlineprogrammer/braintrainer/MainActivity.java
@@ -6,10 +6,15 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
     Button goButton;
+    ArrayList<Integer> answers = new ArrayList<Integer>();
 
 
 
@@ -18,6 +23,42 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         goButton = findViewById(R.id.goButton);
+
+        TextView sumTextView = findViewById(R.id.sumTextView);
+        Button button0 = findViewById(R.id.button0);
+        Button button1 = findViewById(R.id.button1);
+        Button button2 = findViewById(R.id.button2);
+        Button button3 = findViewById(R.id.button3);
+
+        Random rand = new Random();
+
+        int a = rand.nextInt(21);
+        int b = rand.nextInt(21);
+
+        int locationOfCorrectAnswer = rand.nextInt(4);
+
+        sumTextView.setText(Integer.toString(a) + " + " + Integer.toString(b));
+
+        for (int i=0; i<4;i++){
+            if(i==locationOfCorrectAnswer){
+                answers.add(a+b);
+
+            } else {
+                int wrongAnswer = rand.nextInt(41);
+                while (wrongAnswer == a+b){
+                    wrongAnswer = rand.nextInt(41);
+                }
+                answers.add(wrongAnswer);
+
+            }
+
+        }
+
+        button0.setText(Integer.toString(answers.get(0)));
+        button1.setText(Integer.toString(answers.get(1)));
+        button2.setText(Integer.toString(answers.get(2)));
+        button3.setText(Integer.toString(answers.get(3)));
+
     }
 
     public void start(View view){
