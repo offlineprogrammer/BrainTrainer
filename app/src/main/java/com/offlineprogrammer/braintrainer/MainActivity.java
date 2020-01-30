6dfe48a237f40b8268fbbe1834e88aa42cddb673
offlineProgrammer <mmalaka@gmail.com>
2020-01-29 16:25:35 -0800

    Home View

--- app/src/main/java/com/offlineprogrammer/braintrainer/MainActivity.java
+++ app/src/main/java/com/offlineprogrammer/braintrainer/MainActivity.java
@@ -400,6 +400,7 @@ public class MainActivity extends AppCompatActivity {
      */
     private void setLoggedInButtonsVisibility(int visibility) {
         mLogoutButton.setVisibility(visibility);
+
     }
 
     /**
