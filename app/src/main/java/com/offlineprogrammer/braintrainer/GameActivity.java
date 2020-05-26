760eced996f2f80c3b3774b256d3f23b81c96fca
offlineProgrammer <mmalaka@gmail.com>
2020-05-25 18:02:53 -0700

    AMZ Ads

--- app/src/main/java/com/offlineprogrammer/braintrainer/GameActivity.java
+++ app/src/main/java/com/offlineprogrammer/braintrainer/GameActivity.java
@@ -7,6 +7,8 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.os.Bundle;
 import android.util.Log;
 
+import com.amazon.device.ads.AdLayout;
+import com.amazon.device.ads.AdRegistration;
 import com.offlineprogrammer.braintrainer.answer.Answer;
 import com.offlineprogrammer.braintrainer.answer.AnswerAdapter;
 import com.offlineprogrammer.braintrainer.answer.AnswerGridItemDecoration;
@@ -23,6 +25,9 @@ public class GameActivity extends AppCompatActivity implements OnAnswerListener 
     ArrayList<Answer> mAnswerList;
     Answer mAnswer;
     AnswerAdapter myAdapter;
+    private AdLayout adView; // The ad view used to load and display the ad.
+    private static final String APP_KEY = "3967f616abb34b3c9f83c8d4c86eec34"; // Sample Application Key. Replace this value with your Application Key.
+    private static final int INITIALIZATION_TIMEOUT_MS = 2000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +42,7 @@ public class GameActivity extends AppCompatActivity implements OnAnswerListener 
         mRecyclerView.addItemDecoration(new AnswerGridItemDecoration(largePadding, smallPadding));
 
         prepareData();
+        setupAds();
 
     }
 
@@ -52,6 +58,28 @@ public class GameActivity extends AppCompatActivity implements OnAnswerListener 
         myAdapter = new AnswerAdapter(GameActivity.this, mAnswerList,this);
         mRecyclerView.setAdapter(myAdapter);
 
+    }
+
+
+    private void setupAds() {
+
+        AdRegistration.setAppKey("3967f616abb34b3c9f83c8d4c86eec34");
+        AdRegistration.enableLogging(true);
+        // For debugging purposes flag all ad requests as tests, but set to false for production builds.
+        AdRegistration.enableTesting(true);
+        this.adView = findViewById(R.id.ad_view);
+        try {
+            AdRegistration.setAppKey(APP_KEY);
+            loadAd();
+        } catch (final IllegalArgumentException e) {
+            Log.e(TAG, "IllegalArgumentException thrown: " + e.toString());
+            return;
+        }
+
+    }
+
+    public void loadAd() {
+        this.adView.loadAd();
     }
 
     @Override
