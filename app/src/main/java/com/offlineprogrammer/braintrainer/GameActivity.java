1d9e698b67b9293f0525d9bc888537c969c453c7
offlineProgrammer <mmalaka@gmail.com>
2020-05-25 19:44:37 -0700

    Amazon Ads

--- app/src/main/java/com/offlineprogrammer/braintrainer/GameActivity.java
+++ app/src/main/java/com/offlineprogrammer/braintrainer/GameActivity.java
@@ -5,10 +5,25 @@ import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import com.amazon.device.ads.AdLayout;
 import com.amazon.device.ads.AdRegistration;
+import com.amazon.device.iap.PurchasingListener;
+import com.amazon.device.iap.PurchasingService;
+import com.amazon.device.iap.model.FulfillmentResult;
+import com.amazon.device.iap.model.Product;
+import com.amazon.device.iap.model.ProductDataResponse;
+import com.amazon.device.iap.model.PurchaseResponse;
+import com.amazon.device.iap.model.PurchaseUpdatesResponse;
+import com.amazon.device.iap.model.Receipt;
+import com.amazon.device.iap.model.UserDataResponse;
 import com.offlineprogrammer.braintrainer.answer.Answer;
 import com.offlineprogrammer.braintrainer.answer.AnswerAdapter;
 import com.offlineprogrammer.braintrainer.answer.AnswerGridItemDecoration;
@@ -16,7 +31,10 @@ import com.offlineprogrammer.braintrainer.answer.OnAnswerListener;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public class GameActivity extends AppCompatActivity implements OnAnswerListener {
     private static final String TAG = "GameActivity";
@@ -28,6 +46,11 @@ public class GameActivity extends AppCompatActivity implements OnAnswerListener 
     private AdLayout adView; // The ad view used to load and display the ad.
     private static final String APP_KEY = "3967f616abb34b3c9f83c8d4c86eec34"; // Sample Application Key. Replace this value with your Application Key.
     private static final int INITIALIZATION_TIMEOUT_MS = 2000;
+    String parentSKU;
+    Handler handler;
+    //Define UserId and MarketPlace
+    private String currentUserId;
+    private String currentMarketplace;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,6 +66,7 @@ public class GameActivity extends AppCompatActivity implements OnAnswerListener 
 
         prepareData();
         setupAds();
+        setupIAP();
 
     }
 
@@ -58,6 +82,46 @@ public class GameActivity extends AppCompatActivity implements OnAnswerListener 
         myAdapter = new AnswerAdapter(GameActivity.this, mAnswerList,this);
         mRecyclerView.setAdapter(myAdapter);
 
+    }
+
+    private void setupIAP() {
+        parentSKU = "com.offlineprogrammer.braintrainer.removeads";
+
+        PurchasingService.registerListener(this, purchasingListener);
+
+        ImageButton button =  findViewById(R.id.removeAdsButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                PurchasingService.purchase(parentSKU);
+            }
+        });
+
+        //create a handler for the UI changes
+        handler = new Handler(Looper.getMainLooper()) {
+            @Override
+            public void handleMessage(Message msg) {
+                if (msg.obj.equals("RemoveAds")) {
+                    //button.setText("complete");
+                    Log.i(TAG, "handleMessage: Complete");
+                    clearAds();
+
+
+                }
+            }
+        };
+    }
+
+    private void clearAds() {
+
+        this.adView = findViewById(R.id.ad_view);
+        try {
+            this.adView.destroy();
+            Log.i(TAG, "clearAds: done");
+        } catch (final IllegalArgumentException e) {
+            Log.e(TAG, "IllegalArgumentException thrown: " + e.toString());
+            return;
+        }
     }
 
 
@@ -86,4 +150,98 @@ public class GameActivity extends AppCompatActivity implements OnAnswerListener 
     public void onAnswerClick(int position) {
 
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+//getUserData() will query the Appstore for the Users information
+        PurchasingService.getUserData();
+//getPurchaseUpdates() will query the Appstore for any previous purchase
+        PurchasingService.getPurchaseUpdates(true);
+//getProductData will validate the SKUs with Amazon Appstore
+        final Set<String> productSkus = new HashSet<String>();
+        productSkus.add(parentSKU);
+        PurchasingService.getProductData(productSkus);
+
+        Log.v("Validating SKUs", "Validating SKUs with Amazon" );
+
+
+    }
+
+
+
+
+    PurchasingListener purchasingListener = new PurchasingListener() {
+        @Override
+        public void onUserDataResponse(UserDataResponse response) {
+            final UserDataResponse.RequestStatus status = response.getRequestStatus();
+            switch (status) {
+                case SUCCESSFUL:
+                    currentUserId = response.getUserData().getUserId();
+                    currentMarketplace = response.getUserData().getMarketplace();
+                    break;
+                case FAILED:
+                case NOT_SUPPORTED:
+                    // Fail gracefully.
+                    break;
+            }
+        }
+        @Override
+        public void onProductDataResponse(ProductDataResponse productDataResponse) {
+            switch (productDataResponse.getRequestStatus()) {
+                case SUCCESSFUL:
+
+                    //get informations for all IAP Items (parent SKUs)
+                    final Map<String, Product> products = productDataResponse.getProductData();
+                    for ( String key : products.keySet()) {
+                        Product product = products.get(key);
+                        Log.v("Product:", String.format( "Product: %s\n Type: %s\n SKU: %s\n Price: %s\n Description: %s\n" , product.getTitle(), product.getProductType(),
+                                product.getSku(), product.getPrice(), product.getDescription()));
+                    }
+                    //get all unavailable SKUs
+                    for ( String s : productDataResponse.getUnavailableSkus()) {
+                        Log.v("Unavailable SKU:"+s, "Unavailable SKU:" + s);
+                    }
+                    break;
+                case FAILED:
+                    Log.v("FAILED", "FAILED" );
+                    break ;
+            }
+        }
+        @Override
+        public void onPurchaseResponse(PurchaseResponse purchaseResponse) {
+            switch (purchaseResponse.getRequestStatus()) {
+                case SUCCESSFUL:
+                    PurchasingService.notifyFulfillment(purchaseResponse.getReceipt().getReceiptId(),
+                            FulfillmentResult.FULFILLED);
+                    break ;
+                case FAILED:
+                    break ;
+            }
+        }
+        @Override
+        public void onPurchaseUpdatesResponse(PurchaseUpdatesResponse response) {
+            // Process receipts
+            switch (response.getRequestStatus()) {
+                case SUCCESSFUL:
+                    for ( final Receipt receipt : response.getReceipts()) {
+                        // Process receipts
+                        if (!receipt.isCanceled()){
+                            Message m= new Message();
+                            m.obj="RemoveAds";
+                            handler.handleMessage(m);
+                        }
+                    }
+                    if (response.hasMore()) {
+                        PurchasingService.getPurchaseUpdates(true);
+                    }
+                    break ;
+                case FAILED:
+                    Log.d("FAILED","FAILED");
+                    break ;
+            }
+
+        }
+    };
 }
